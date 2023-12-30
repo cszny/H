e4b50f599c03df1fc1057f8e568a479ff1365d07
cszn.dev@gmail.com <cszn.dev@gmail.com>
2023-12-30 16:08:21 +0800

    hihi

---
+++ api/app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request, redirect, url_for, g, flash, abort, jsonify, send_file, Response, make_response
+import requests
+import base64
+import hashlib
+import time
+import socket
+import threading
+import random
+import string
+import os
+import re
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "Hikari"
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+  return "Hello"
+  
+@app.route("/library", methods=["GET", "POST"])
+def library():
+  filePath = "../ds/gx"
+  fileOpen = open(filePath, 'r')
+  fileRead = fileOpen.read()
+  return Response(fileRead, content_type="text/plain")
